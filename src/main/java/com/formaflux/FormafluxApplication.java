672295bae83ccec12630584355798cf486f83a6b
Avinash Rapolu <avinash675423@gmail.com>
2025-12-05 10:45:38 +0530

    initial

---
+++ src/main/java/com/formaflux/FormafluxApplication.java
@@ -0,0 +1,13 @@
+package com.formaflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormafluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormafluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formaflux/FormafluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormafluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
